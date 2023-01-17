0ec2b01cbcbe0f2188ed91382b5c7d6088a72d55
Алексей Вовченко <blackpantific@gmail.com>
2023-01-17 20:57:48 +0300

    fix possibility to send post, put, delete requests

---
+++ src/main/kotlin/com/example/kotlinspring/WebSecurityConfig.java
@@ -0,0 +1,18 @@
+package com.example.kotlinspring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig{
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception{
+        http.cors().and().csrf().disable();
+        return http.build();
+    }
+}
